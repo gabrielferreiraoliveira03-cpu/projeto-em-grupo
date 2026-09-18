5feb24e761c69297ee54f19c35c8a67227b30665
gabrielferreiraoliveira03-cpu <gabriel.ferreira.oliveira03@escola.pr.gov.br>
2026-09-18 08:27:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,113 @@
-const elementoPergunta = document.querySelector("#pergunta");
-const elementoAlternativas = document.querySelector("#alternativas");
-const elementoResultado = document.querySelector("#resultado");
-
-let perguntaAtual = 0;
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-
     {
-        pergunta: "Você está em uma floresta. O que deseja fazer?",
-
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Seguir pela trilha",
-                proxima: 1
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Entrar na caverna",
-                proxima: 2
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
-
-
     {
-        pergunta: "Você encontrou uma ponte. O que deseja fazer?",
-
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Atravessar a ponte",
-                proxima: 3
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Procurar outro caminho",
-                proxima: 4
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
-
-
     {
-        pergunta: "Dentro da caverna existe uma luz misteriosa. Você se aproxima?",
-
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "Sim, vou investigar",
-                proxima: 3
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-                texto: "Não, vou embora",
-                proxima: 4
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
-
-
     {
-        pergunta: "Você chegou a um castelo misterioso. O que deseja fazer?",
-
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "Entrar no castelo",
-                proxima: 5
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
             },
             {
-                texto: "Continuar pela floresta",
-                proxima: 4
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
         ]
     },
-
-
     {
-        pergunta: "Você encontrou uma pequena vila. Os moradores podem ajudar.",
-
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
-                texto: "Pedir ajuda",
-                proxima: 5
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "Continuar sozinho",
-                proxima: 5
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
     },
-
-
-    {
-        pergunta: "Dentro do castelo você encontrou o tesouro! Você completou sua missão.",
-
-        alternativas: []
-    }
-
 ];
 
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
 function mostraPergunta() {
-
-    const pergunta = perguntas[perguntaAtual];
-
-    elementoPergunta.textContent = pergunta.pergunta;
-
-    elementoAlternativas.innerHTML = "";
-
-    elementoResultado.textContent = "";
-
-    if (pergunta.alternativas.length === 0) {
-
-        elementoResultado.textContent = "🏆 Fim da missão!";
-
+    if (atual >= perguntas.length) {
+        mostraResultado();
         return;
     }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
 
-    pergunta.alternativas.forEach(alternativa => {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
 
-        const botao = document.createElement("button");
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
 
-        botao.textContent = alternativa.texto;
-
-        botao.classList.add("botao-alternativa");
-        botao.addEventListener("click", () => {
-
-            perguntaAtual = alternativa.proxima;
-
-            mostraPergunta();
-
-        });
-
-        elementoAlternativas.appendChild(botao);
-
-    });
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
